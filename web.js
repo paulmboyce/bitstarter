0ca62d0b3fcec5fbd5763141d227a93de5e75bf1
Paul Boyce <paul.boyce@goobito.com>
2013-08-07 11:38:42 +0000

    Added index file and read content in web server

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello awesome World!');
+//  response.send('Hello awesome World!  Dude this is totally awesmoe!!! My first web server!!!!');
+
+    var data = fs.readFileSync("index.html", "utf8");
+    response.send (data);
+
 });
 
 var port = process.env.PORT || 8080;
